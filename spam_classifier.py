0a063d5e26dd3917ace6df2c06ba318e84b2aac3
Vinh Ta <vinhta@Vinhs-MBP.T-mobile.com>
2018-03-31 21:23:41 -0700

    spam_classifier datapreprocessing

--- spam_classifier.py
+++ spam_classifier.py
@@ -1,3 +1,41 @@
 #python 3
 #building a spam classifier using sklearn
 
+import os
+import io
+import numpy as np
+from pandas import DataFrame
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+def readFiles(path):
+    for root, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            path = os.path.join(root, filename)
+            
+            inBody = False
+            lines = []
+            f = io.open(path,'r',encoding='latin1')
+            
+            for line in f:
+                if inBody:
+                    lines.append(line)
+                elif line == '\n':
+                    inBody = True
+                
+            f.close()
+            message = '\n'.join(lines)
+            yield path, message
+        
+def dataFrameFromDirectory(path,classification):
+    rows = []
+    index = []
+    for filename, message in readFiles(path):
+        rows.append({'message': message, 'class': classification})
+        index.append(filename)
+    
+    return DataFrame(rows,index=index)
+
+data = DataFrame({'message':[], 'class':[]})
+data = data.append(dataFrameFromDirectory('/Users/vinhta/DS/sundog/Predictive_modelling/emails/spam', 'spam'))
+data = data.append(dataFrameFromDirectory('/Users/vinhta/DS/sundog/Predictive_modelling/emails/ham','ham'))        
